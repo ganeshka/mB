ebfe149a00269b23c5c8c74910dbe2e4d7cf8848
ganeshka <ganesh.sh.kamble@gmail.com>
2012-10-17 14:10:22 +0530

    Added login.html

--- mB/src/com/mb/MainActivity.java
+++ mB/src/com/mb/MainActivity.java
@@ -1,20 +1,22 @@
 package com.mb;
 
+import org.apache.cordova.DroidGap;
+
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends DroidGap {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// setContentView(R.layout.activity_main);
+		super.loadUrl("file:///android_asset/www/login.html");
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
 }
